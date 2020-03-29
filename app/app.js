7590ed1f8b4fe2b538351b8c473907075c6459ee
Ryan Roderick <ryan@designmonster.com>
2020-03-29 12:32:31 -0400

    Boilerplate
    notes

--- app/app.js
+++ app/app.js
@@ -4,9 +4,31 @@ import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
 
 export default class App extends Application {
-  modulePrefix = config.modulePrefix;
-  podModulePrefix = config.podModulePrefix;
-  Resolver = Resolver;
+	modulePrefix = config.modulePrefix;
+
+	Resolver = Resolver.extend({
+		/** Extend the resolver to allow routes to be within the routes directory.*/
+		adjustRoute(parsedName, type) {
+			parsedName.fullNameWithoutType = 'routes/' + parsedName.fullNameWithoutType;
+			parsedName.fullName = type + ':' + parsedName.fullNameWithoutType;
+			return this;
+		},
+		resolveRoute(parsedName) {
+			return this._super(parsedName) || this.adjustRoute(parsedName, 'route')._super(parsedName);
+		},
+		resolveTemplate(parsedName) {
+			return this._super(parsedName) || this.adjustRoute(parsedName, 'template')._super(parsedName);
+		},
+		resolveController(parsedName) {
+			return this._super(parsedName) || this.adjustRoute(parsedName, 'controller')._super(parsedName);
+		},
+		resolveAdapter(parsedName) {
+			return this._super(parsedName) || this.adjustRoute(parsedName, 'adapter')._super(parsedName);
+		},
+		resolveSerializer(parsedName) {
+			return this._super(parsedName) || this.adjustRoute(parsedName, 'serializer')._super(parsedName);
+		},
+	});
 }
 
 loadInitializers(App, config.modulePrefix);

8945d3b7511f70f2b9f290d96adeb8c8d1ae9400
Matthew <roderickdevelopers@gmail.com>
2020-08-18 16:14:31 -0400

    upgrade_octane_3.20.0

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,5 +1,5 @@
-import Application from '../app';
-import config from '../config/environment';
+import Application from 'designmonster/app';
+import config from 'designmonster/config/environment';
 import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
 

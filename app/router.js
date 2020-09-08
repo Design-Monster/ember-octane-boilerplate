22030bfeaf5bb219d1cd9505995dfe9563e35365
Ryan Roderick <ryan@designmonster.com>
2020-09-07 20:06:34 -0400

    added some clariying examples for structure and ember-responsive

--- app/router.js
+++ app/router.js
@@ -1,7 +1,7 @@
-import EmberRouter from '@ember/routing/router';
+import EmberRouterScroll from '@ember/routing/router';
 import config from 'ember-octane-boilerplate/config/environment';
 
-export default class Router extends EmberRouter {
+export default class Router extends EmberRouterScroll {
   location = config.locationType;
   rootURL = config.rootURL;
 }

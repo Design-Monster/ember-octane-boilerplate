9e0437bb9e152a5f9851c699dec4b6909d99660a
Ryan Roderick <ryan@designmonster.com>
2023-06-13 14:58:12 -0400

    fixed ember responsive with 5.0 no implicit injections

--- app/routes/application/route.js
+++ app/routes/application/route.js
@@ -1,3 +1,5 @@
 import Route from '@ember/routing/route';
-
-export default class ApplicationRoute extends Route {}
+import { inject as service } from '@ember/service';
+export default class ApplicationRoute extends Route {
+  @service media;
+}
